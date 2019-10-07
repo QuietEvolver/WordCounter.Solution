10558de027953d07f13fd3c7ec47a61251e73405
Vera Weikel <vera.weikel@gmail.com>
2019-10-06 22:33:28 -0700

    Update to simplify logic: only Sentence to be evalulated.

--- WordCounter/Model/WordCounter.cs
+++ WordCounter/Model/WordCounter.cs
@@ -7,23 +7,58 @@ namespace WordCounter.Models
 {
     public class Counter 
     {
-        
-        public static int count = 0; //numOfWord = 0;
+        public static int Count = 0; //numOfWord = 0;
         public string Word { get; set; } //public char[] ToCharArray()
-        public string Sentence { get; set; } //public char[]  ToCharArray(int startIndex, int length)
-        public char[] StrToCharArr { get; set; }
+        public char[] WordStrToCharArr { get; set; }  
+        public string Sentence { get; set; } 
+        // public char[] SentenceStrToCharArr { get; set; }  
+        public char[] CharSentenceWordToCharArr { get; set; }  
+        
+        //public char[]  ToCharArray(int startIndex, int length)
                 
-        public Counter ( string word, string sentence , char[] strToCharArr)
+        public Counter ( string word, string sentence )//, char[] wordStrToCharArr)
         {
             Word = word;
             Sentence = sentence;  
-            StrToCharArr = strToCharArr;
+            // WordStrToCharArr = Word.ToCharArray();
+            // SentenceStrToCharArr = Sentence.ToCharArray();
+            // //SentenceStrToCharArr = SentenceWord.ToCharArray();
+            // Console.WriteLine("WordStrToCharArr" + WordStrToCharArr + "/SentenceStrToCharArr: " + SentenceStrToCharArr);
+            // Count = 0;
         }
-     
-        //public virtual bool Equals(object word) //public char[] Word
-
-       // public Sent 
+         public int WordCount( )
+         {
+            int Count = 0; 
+          // Separator =;
+            string[] SentenceStrToArr = Sentence.Split(new Char[]  { ' ', '.', ',', '/', '\n', '!', '?'}); 
+            for(int i = 0; i < SentenceStrToArr.Length; i++)
+            {
+                if( Word == SentenceStrToArr[i])
+                {
+                    Count++;
+                }
+            }
+            return Count;
+        }
+            // public Sent SentWordStrToCharArr = sentWrdStrToCharArr; public char[] SentWordStrToCharArr [i]{ get; set; } 
+            //public virtual bool Equals(object word) //public char[] Word
+            // public bool IsWord(int Count)
+            // {
+            //     bool result = true;
+            //     return result;
+            //     // for(int i = 0; i < WordStrToCharArr.Length; i++)
+            //     // {
+            //     //     string[] SentenceStrToCharArr = this.resultSentenceParse;
+            //     //     if(!(Char.IsLetterWordStrToCharArr[i] == (Char.IsLetter(CharSentenceWordToCharArr[i])))
+            //     //     {
+                        
+            //     //         Count = SentenceWordStrToCharArr.Equals(Word)? ++Count: Count;//.Next();
+            //     //     }
+            //     //
+            //     // return Count;
+            // //     // Console.WriteLine("EvalWord()isLetter: " + );
+            // //     WordStrToCharArr.IsLetter(0, 0);
+            // }
     }
 
-   // public class Word 
 }
--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -3,30 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using WordCounter.Models; 
 
-namespace WordCounter.WordCounter
+namespace WordCounter
 {
     public class Program
     {
         public static void Main()
-        {
+        {   
+            List <Counter> BaseCounter = new List <Counter>(){};
             Console.WriteLine("Welcome to WordCounter");
             Console.WriteLine("Enter in a word: ");
-            string newWord = Console.ReadLine();
-            // char[] newWordArr = newWord.ToCharArray(
-            //    // char[] newWordArr = new char[0][]{
-            // //}
-            // );
+            string Word = Console.ReadLine();            
+            Console.WriteLine("Enter in a sentence: ");
+            string Sentence = Console.ReadLine();
             
-            // //char[] WordArr =  
-            // char[] res = newWordArr.ToDictionary(item => item);// item.Value.ToCharArray());
-            // 
-               // WordArr = Word.ToCharArray();
-              // char[]  WordCharArr = newWord.ToCharArray(Console.ReadLine());
-            //newWord = Word;//str
-          
-            //char[] Word = newWord0][newWord.length-1]; //inputStr.ToUpper(0, (inputStr.length-1));//0, (inputStr.length-1));
-            //  char[] inputStr = input;
-            Console.WriteLine("Input Str: " + newWord); //+ "\nWord: " + Word);
+            Counter newBaseCounter = new Counter(Word, Sentence);
+            int result = newBaseCounter.WordCount();           
+            char[]  WordCharArr =  (Word.ToUpper()).ToCharArray();
+            bool resultWordChar; 
+            resultWordChar = Char.IsLetter(WordCharArr[0]);
+            Console.WriteLine("rez resultWordChar: " +  resultWordChar + "weighted result: " + result);
+
+        //    // string[] SentenceSplit 
+        //     bool resultSentenceParse; 
+
+        //     char[]  resultSentenceParse =  (Sentence.ToUpper()).ToCharArray(); //CharSentenceArr
+      
+            Console.WriteLine("Input Str: " + "List: newBaseCounter/newBaseCounter.WordCharArr "+ newBaseCounter +  newBaseCounter.WordStrToCharArr + "\nWord: " + Word + "\nWordCharArr: " + WordCharArr[0]);
+        //     Console.WriteLine("Input Str: " + "List: newBaseCounter.SentenceCharArr "+   newBaseCounter.SentenceStrToCharArr + "\nSentence: " + Sentence + "\resultSentenceParse: " + resultSentenceParse[0]);
+            
         }
 
     }
